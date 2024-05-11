171604a99cce3421353362331b415ea12f5946fe
Matthew Lopez <719710@student.dusd.net>
2024-05-10 23:04:54 -0700

    Working on Lab 3
    figuring out how to get player movement still

--- PersonalProjectLopezP5/Assets/Scripts/PlayerController.cs
+++ PersonalProjectLopezP5/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float verticalInput;
-    public float horizontalInput;
-    public float speed = 10.0f;
-    private Rigidbody playerRb;
+    public float moveSpeed = 5f;
+    public float lookSpeed = 2f;
 
+    private Rigidbody rb;
+    private Vector3 movement;
+    private Vector3 rotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Player Movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        movement = transform.right * horizontalInput + transform.forward * verticalInput;
+
+        //Player Rotation
+        float mouseX = Input.GetAxis("Mouse X") * lookSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * lookSpeed;
+        rotation = new Vector3(-mouseY, mouseX);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        Quaternion deltaRotation = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
     }
 }
